3a99511098121462c04e6cfe7699b16fa21be3ca
oleghasjanov <oleg.phenomenon@gmail.com>
2025-09-23 18:45:13 +0300

    implemented price listener

--- app/services/binance/price_listener.rb
+++ app/services/binance/price_listener.rb
@@ -1,29 +1,33 @@
-require 'faye/websocket'
-require 'eventmachine'
-require 'json'
-
+require "faye/websocket"
+require "eventmachine"
+require "json"
+require "redis"
+# require 'sidekiq'
 module Binance
   class PriceListener
-    # WebSocket URL for all tickers
     WEBSOCKET_URL = "wss://stream.binance.com:9443/ws/!ticker@arr"
+
+    attr_reader :redis
+
+    def initialize
+      @redis = Redis.new(url: ENV["REDIS_URL"])
+    end
 
     def run
       EM.run do
         ws = Faye::WebSocket::Client.new(WEBSOCKET_URL)
 
         ws.on :open do |event|
-          p [:open, "Connected to Binance WebSocket"]
+          p [ :open, "Connected to Binance WebSocket" ]
         end
 
         ws.on :message do |event|
           data = JSON.parse(event.data)
-          data.each do |ticker|
-            process_ticker(ticker)
-          end
+          data.each { |ticker| process_ticker(ticker) }
         end
 
         ws.on :close do |event|
-          p [:close, event.code, event.reason]
+          p [ :close, event.code, event.reason ]
           ws = nil
         end
       end
@@ -32,12 +36,32 @@ module Binance
     private
 
     def process_ticker(ticker)
-      symbol = ticker['s']
-      price = ticker['c']
-      
-      puts "Symbol: #{symbol}, Price: #{price}"
-      
-      # TODO: check alerts in Redis
+      symbol = ticker["s"]
+      price = BigDecimal(ticker["c"])
+      redis_key = "alerts:binance:#{symbol}"
+      alerts_to_check = @redis.hgetall(redis_key)
+
+      return if alerts_to_check.empty?
+
+      alerts_to_check.each do |alert_id, condition|
+        threshold_str, direction = condition.split("_")
+        threshold = BigDecimal(threshold_str)
+
+        price_crossed_up = (direction == "up" && price > threshold)
+        price_crossed_down = (direction == "down" && price < threshold)
+
+        if price_crossed_up || price_crossed_down
+          puts "!!! ALERT TRIGGERED: #{symbol} price #{price} crossed #{direction} #{threshold} (Alert ID: #{alert_id})"
+
+          # Sidekiq::Client.push(
+          #   'queue' => 'default',
+          #   'class' => 'NotificationJob',
+          #   'args' => [alert_id.to_i]
+          # )
+
+          @redis.hdel(redis_key, alert_id)
+        end
+      end
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module CryptoTracker
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
 
     # Configuration for the application, engines, and railties goes here.
     #
