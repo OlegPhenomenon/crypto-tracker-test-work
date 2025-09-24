a44941c1ab18376de4d10e5f1cf8b80fd76a25a3
oleghasjanov <oleg.phenomenon@gmail.com>
2025-09-24 13:19:22 +0300

    refactoring

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,2 +1,9 @@
 module WorkspacesHelper
+  def colorize(color)
+    if color
+      "bg-#{color}-100 text-#{color}-600 group-hover:bg-#{color}-200"
+    else
+      "bg-gray-100 text-gray-600 group-hover:bg-gray-200"
+    end
+  end
 end
